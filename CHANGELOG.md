a1a4ed1b3439bb3ef98e8847a287a71d98e95b1b
Mladen Jablanovic <jablan@radioni.ca>
2019-04-07 18:15:11 +0200

    v0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## [0.2.0] - 2019-04-07
+
+* `scroll(str, x, y, len)` for scrolling text
+
+## [0.1.0] - 2019-04-05
+
+* Initial commit
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Pcd8544.MixProject do
     [
       app: :pcd_8544,
       description: "Elixir driver for PCD8544 based LCD displays",
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
